a83193c663176b02357a6682b87b8705f2ca1ee2
Empty <emptypageworking@gmail.com>
2026-09-07 07:19:03 +0700

    feat(settings): delete storybord
    + deleted storybord
    + setting SceneDelegate (init window)

---
+++ Weather-App/Weather-App/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Weather-App
+//
+//  Created by Valera on 31.08.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
+
